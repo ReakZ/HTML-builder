0c7505fb62a5d76dc9a33c6aae0bf0671bd552c1
ReakZ <spec.kov@list.ru>
2022-05-21 12:11:54 +0300

    feat: implement task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const { readdir, access, mkdir, copyFile } = require('fs/promises');
+const path = require('path');
+
+const dir = path.join(__dirname, 'files');
+const copyDir = path.join(__dirname, 'files-copy');
+
+async function copyFiles() {
+  try {
+    await access(copyDir);
+  } catch (error) {
+    if (error.code == 'ENOENT') {
+      try {
+        await mkdir(copyDir);
+      } catch (error) {
+        console.log(error, 'cant create');
+      }
+    }
+  }
+  try {
+    const filesInDir = await readdir(dir, { withFileTypes: true });
+
+    filesInDir.forEach(async (x) => {
+      if (x.isFile()) {
+        await copyFile(path.join(dir, x.name), path.join(copyDir, x.name));
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+copyFiles();
 
