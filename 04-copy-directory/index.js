ca4bc3115775e23f9e4542f6cf9b6251e0265122
ReakZ <spec.kov@list.ru>
2022-05-23 01:34:01 +0300

    fix: rework function in task 04-copy-directory
    rework delete dir and create new dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,36 +1,29 @@
-const { readdir, access, mkdir, copyFile } = require('fs/promises');
+const { readdir, mkdir, copyFile, rmdir } = require('fs/promises');
 const path = require('path');
 
 const dir = path.join(__dirname, 'files');
 const copyDir = path.join(__dirname, 'files-copy');
 
-async function copyFiles(fromDir,toDir) {
+async function copyFiles(fromDir, toDir) {
   try {
-    await access(toDir);
-  } catch (error) {
-    if (error.code == 'ENOENT') {
-      try {
-        await mkdir(toDir);
-      } catch (error) {
-        console.log(error, 'cant create');
-      }
+    await mkdir(toDir);
+  } catch (err) {
+    if (err.code == 'EEXIST') {
+      await rmdir(toDir, { recursive: true });
+      await mkdir(toDir);
     }
-  }
-  try {
+  } finally {
     const filesInDir = await readdir(fromDir, { withFileTypes: true });
 
     filesInDir.forEach(async (x) => {
       if (x.isFile()) {
         await copyFile(path.join(fromDir, x.name), path.join(toDir, x.name));
       }
-      if(x.isDirectory()){
+      if (x.isDirectory()) {
         await copyFiles(path.join(fromDir, x.name), path.join(toDir, x.name));
       }
     });
-  } catch (err) {
-    console.log(err);
   }
 }
 
-copyFiles(dir,copyDir);
-
+copyFiles(dir, copyDir);
