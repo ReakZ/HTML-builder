7a515c5f74e33613742841810b9c1c13cf9480df
ReakZ <spec.kov@list.ru>
2022-05-23 11:37:37 +0300

    fix: change copy function in 06-build-page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,6 +6,7 @@ const {
   access,
   mkdir,
   copyFile,
+  rmdir
 } = require('fs/promises');
 const path = require('path');
 
@@ -130,17 +131,13 @@ async function packStyles(stylesPath, pathToBundleFile) {
 
 async function copyFiles(fromDir, toDir) {
   try {
-    await access(toDir);
-  } catch (error) {
-    if (error.code == 'ENOENT') {
-      try {
-        await mkdir(toDir);
-      } catch (error) {
-        console.log(error, 'cant create');
-      }
+    await mkdir(toDir);
+  } catch (err) {
+    if (err.code == 'EEXIST') {
+      await rmdir(toDir, { recursive: true });
+      await mkdir(toDir);
     }
-  }
-  try {
+  } finally {
     const filesInDir = await readdir(fromDir, { withFileTypes: true });
 
     filesInDir.forEach(async (x) => {
@@ -151,7 +148,5 @@ async function copyFiles(fromDir, toDir) {
         await copyFiles(path.join(fromDir, x.name), path.join(toDir, x.name));
       }
     });
-  } catch (err) {
-    console.log(err);
   }
 }
